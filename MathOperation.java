150c6ae74b8f84bda6c7ea591098ad9f34c62267
Parvsub <Praveenboda421@gmail.com>
2023-03-11 12:07:50 +0530

    Added: To show the resultof Math Operation

--- MathOperation.java
+++ MathOperation.java
@@ -1,20 +1,26 @@
 package com.lamdaexpressions;
 @FunctionalInterface
-interface MathFunction{
-    int calculation(int a,int b);
-}
+interface MathFunction {
+    int calculation(int a, int b);
 
-public class MathOperation {
-    public static void main(String[] args) {
+    static void printResult(int a, int b, String function, MathFunction fobj) {
+        System.out.println("Result of " + function + " is " + fobj.calculation(a, b));
+    }
 
-        MathFunction addition = (a,b) -> a+b;
-        MathFunction subtraction = (a,b) -> a-b;
-        MathFunction multiplication = (a,b) -> a*b;
-        MathFunction division = (a,b) -> a/b;
+    public class MathOperation {
+        public static void main(String[] args) {
 
-        System.out.println("Addition "+ addition.calculation(2,3));
-        System.out.println("Subtraction  "+ subtraction.calculation(5,3));
-        System.out.println("Division "+ division.calculation(10,2));
+            MathFunction addition = (a, b) -> a + b;
+            MathFunction subtraction = (a, b) -> a - b;
+            MathFunction multiplication = (a, b) -> a * b;
+            MathFunction division = (a, b) -> a / b;
 
+            System.out.println("Addition " + addition.calculation(2, 3));
+            System.out.println("Subtraction  " + subtraction.calculation(5, 3));
+            System.out.println("Multiplication  " + subtraction.calculation(6, 8));
+            System.out.println("Division " + division.calculation(10, 2));
+
+            MathFunction.printResult(4, 2, "addition", addition);
+        }
     }
 }
